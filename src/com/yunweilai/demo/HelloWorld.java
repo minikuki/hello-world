55e9934621939657536639b7822fe853935c1968
yuning ma <yuning.ma85.tw@gmail.com>
2019-03-07 15:51:10 +0800

    第一次

---
+++ src/com/yunweilai/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.yunweilai.demo;
+
+/**
+ * Create on 2019/3/7 0007 15:09
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
